00a6f3e10cb9962cd9534dd6a6d2e9e99aa68f81
snehapriy958 <snehapriy958@gmail.com>
2026-05-07 00:10:22 +0530

    dsa arrays3

---
+++ arrays/merge_sorted_array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, nums1: list, m: int, nums2: list, n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2[:]
+        nums1.sort()
+
+if __name__ == '__main__':
+    nums1 = [1,2,3,0,0,0]
+    m = 3
+    nums2 = [2,5,6]
+    n = 3
+    obj = Solution()
+    obj.merge(nums1,m,nums2,n)
+    print(nums1)
